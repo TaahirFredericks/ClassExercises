6fa1e45273fef25d34beb4e1bb78f9c018d52602
TaahirFredericks <152262808+TaahirFredericks@users.noreply.github.com>
2023-11-28 12:23:14 +0200

    first commit

---
+++ FindSecondBiggestNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace FindSecondBiggestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mynumbers = { 9, 1, 7, 4, 2, 5 };
+            int SecondBiggest = NumberStyles.SecondBiggestNumber(mynumbers);
+            Console.WriteLine(SecondBiggest);
+        }
+    }
+}
---
+++ FindSecondBiggestNumber/SecondNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindBiggestNumber
+{
+    internal static class Numbers
+    {
+        public static int BiggestNumber(int[] numbers)
+        {
+            //int[] mynumbers = { 2, 1, 7, 4, 9, 5 };
+            //Array.Sort(numbers);
+
+            int current = numbers[0];
+            //int current = numbers.Min(x => x);
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > current)
+                    current = numbers[i];
+            }
+            return current;
+
+            /*return numbers.Max();
+            return numbers.Min();*/
+            {
+                Public static int SecondBiggestNumber(int[] numbers);
+            }
+
+            {
+                int biggestnumber = SecondBiggestNumber(numbers);
+                int SecondBiggestNumber = 0;
+                //int Min = Math.Min(numbers[0], numbers[1];
+                for (int i = 0; i < numbers.Length; i++) ;
+            }
+            {
+                if (numbers[i] < BiggestNumber && numbers[i] > SecondBiggestNumber) ;
+                    SecondBiggestNumber = numbers[i]; 
+            }
+            return SecondBiggestNumber;
+
+
+
+
+
+
+
+        }
